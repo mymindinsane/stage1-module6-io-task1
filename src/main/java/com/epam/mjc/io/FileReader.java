5823564b57666ea2abd5b72d638b4cbbe7815a33
mymindinsane <arturguletskiy21@gmail.com>
2024-07-02 10:37:06 +0300

    fix code smell

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,23 +6,22 @@ import java.io.*;
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
+    public Profile getDataFromFile(File file){
         String line = "";
-        int c = 0;
+        int c;
         try (FileInputStream fis = new FileInputStream(file)) {
             while ((c = fis.read()) != -1){
                 line += String.valueOf((char) c);
             }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (IOException e)  {
+            throw new profileInvalidData(e.getMessage());
         }
+
         String[] array = line.split("\n");
         String name = array[0].substring(array[0].lastIndexOf(':') + 1).trim();
         Integer age = Integer.valueOf(array[1].substring(array[1].lastIndexOf(':') + 1).trim());
         String email = array[2].substring(array[2].lastIndexOf(':') + 1).trim();
         Long phone = Long.valueOf(array[3].substring(array[3].lastIndexOf(':') + 1).trim());
-        System.out.println(name);
         return new Profile(name,age,email,phone);
     }
 }
---
+++ src/main/java/com/epam/mjc/io/profileInvalidData.java
@@ -0,0 +1,9 @@
+package com.epam.mjc.io;
+
+
+public class profileInvalidData extends RuntimeException {
+    profileInvalidData (String message) {
+        super(message);
+    }
+
+}
